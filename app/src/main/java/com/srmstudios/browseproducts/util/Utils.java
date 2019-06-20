4a722d4441c7e9b52c831b995691c79269b33022
Shahrukh <shahrukh@mobiwhiz.com>
2019-06-20 05:26:51 +0500

    pdf opening fix for devices below nougat

--- app/src/main/java/com/srmstudios/browseproducts/util/Utils.java
+++ app/src/main/java/com/srmstudios/browseproducts/util/Utils.java
@@ -1,10 +1,8 @@
 package com.srmstudios.browseproducts.util;
 
 import android.app.Activity;
-import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -15,16 +13,12 @@ import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
-import android.provider.DocumentsContract;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.core.content.FileProvider;
-
-import com.srmstudios.browseproducts.R;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -338,23 +332,24 @@ public class Utils {
     }
 
     public static void openPDFIntent(Context context,String filePath){
-        Intent intent;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        try {
+            Intent intent;
             File file=new File(filePath);
-            Uri uri = FileProvider.getUriForFile(context, context.getPackageName() + ".provider", file);
             intent = new Intent(Intent.ACTION_VIEW);
-            //intent.setData(uri);
+            Uri uri;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                uri = FileProvider.getUriForFile(context, context.getPackageName() + ".provider", file);
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            } else {
+                uri = Uri.fromFile(file);
+            }
             intent.setDataAndType(uri, "application/pdf");
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        } else {
-            intent = new Intent(Intent.ACTION_VIEW);
-            intent.setDataAndType(Uri.parse(filePath), "application/pdf");
-            intent = Intent.createChooser(intent, "Open File");
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        }
-        if (intent.resolveActivity(context.getPackageManager()) != null) {
-            context.startActivity(intent);
+            if (intent.resolveActivity(context.getPackageManager()) != null) {
+                context.startActivity(intent);
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
         }
     }
 
